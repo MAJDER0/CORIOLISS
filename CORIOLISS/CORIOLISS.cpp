4544e07be169c16c4e5cdd4e3a84d5c38002c6b6
MAJDER0 <tomasz.majder89@gmail.com>
2025-09-10 22:59:39 +0200

    SDL empty window

---
+++ CORIOLISS/CORIOLISS.cpp
@@ -0,0 +1,56 @@
+﻿#include "CORIOLISS.h"
+#include <SDL.h>
+#include <glad/glad.h>
+#include <cstdio>
+
+using namespace std;
+
+int main(int, char**) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+        std::fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_Window* win = SDL_CreateWindow(
+        "CORIOLISS",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if (!win) { std::fprintf(stderr, "CreateWindow: %s\n", SDL_GetError()); return 1; }
+
+    SDL_GLContext glctx = SDL_GL_CreateContext(win);
+    if (!glctx) { std::fprintf(stderr, "CreateContext: %s\n", SDL_GetError()); return 1; }
+    SDL_GL_MakeCurrent(win, glctx);
+    SDL_GL_SetSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        std::fprintf(stderr, "Failed to load OpenGL via GLAD\n");
+        return 1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = false;
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+        }
+
+        int w, h; SDL_GL_GetDrawableSize(win, &w, &h);
+        glViewport(0, 0, w, h);
+        glClearColor(0.10f, 0.10f, 0.12f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(win);
+    }
+
+    SDL_GL_DeleteContext(glctx);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
